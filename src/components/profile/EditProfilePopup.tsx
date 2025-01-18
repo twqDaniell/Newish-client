bb7e486d3f4c5c81d429a0fbb4781f1bbcc453eb
twqDaniell <thewickedqueend@gmail.com>
2025-01-18 22:58:24 +0200

    edit profile modal design

--- src/components/profile/EditProfilePopup.tsx
+++ src/components/profile/EditProfilePopup.tsx
@@ -1,13 +1,24 @@
-import React, { useEffect } from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useAppContext } from "../../contexts/AppContext.ts";
+import "./EditProfilePopup.css";
 
 export default function EditProfilePopup({ openPopup, setOpenPopup }) {
+  const { user, setUser } = useAppContext();
+
+  const [email, setEmail] = useState(user.email);
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState(user.name);
+  const [phoneNumber, setPhoneNumber] = useState(user.phoneNumber);
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => { setOpen(false); setOpenPopup(false); };
+  const handleClose = () => {
+    setOpen(false);
+    setOpenPopup(false);
+  };
 
   useEffect(() => {
     if (openPopup) {
@@ -17,32 +28,67 @@ export default function EditProfilePopup({ openPopup, setOpenPopup }) {
 
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={
-            {
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                width: 400,
-                bgcolor: 'background.paper',
-                border: '2px solid #000',
-                boxShadow: 24,
-                p: 4,
-            }
-        }>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            borderRadius: 1,
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+            Edit Profile
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+
+          <div className="editForm">
+            {/* Form Fields */}
+            <label>Name *</label>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="inputField"
+              required={true}
+            />
+
+            <label>Email *</label>
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <label>Phone Number *</label>
+            <input
+              type="number"
+              placeholder="Enter your phone number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+
+            <label>New Password (Optional)</label>
+            <input
+              type="password"
+              placeholder="Enter your new password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <Button sx={{ textTransform: "none" }} variant="contained" className="saveEditButton" onClick={handleClose}>Save</Button>
         </Box>
       </Modal>
     </div>
